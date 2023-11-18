2917737e539a5fa4d5d404fe3a6f00f8941071aa
Sobang419 <109142073+Sobang419@users.noreply.github.com>
2023-11-18 16:52:22 +0900

    Update saaple.py

--- saaple.py
+++ saaple.py
@@ -4,7 +4,7 @@ import altair as alt
 
 @st.cache
 def load_data():
-    return pd.read_csv('https://raw.githubusercontent.com/Sobang419/news_visualization/main/sample.csv')
+    return pd.read_csv('https://raw.githubusercontent.com/Sobang419/news_visualization/main/sample_2.csv')
 
 df = load_data()
 
@@ -25,10 +25,10 @@ else:
     # 카테고리별 감정 레이블링 분포 계산
     category_sentiment_distribution = filtered_data.groupby(['aspect', 'sentiment']).size().reset_index(name='counts')
 
-    # 누적 막대 차트 그리기
+    # 가로 막대 차트 그리기
     chart = alt.Chart(category_sentiment_distribution).mark_bar().encode(
-        x=alt.X('aspect:N', title='Category'),
-        y=alt.Y('counts:Q', title='Number of News Items', stack='zero'),
+        x=alt.X('aspect:N', title='Category', axis=alt.Axis(labelAngle=0)), # X축 레이블 가로 배치
+        y=alt.Y('counts:Q', title='Number of News Items', stack='zero', scale=alt.Scale(domain=[0, 15])), # Y축 범위 설정
         color=alt.Color('sentiment:N', scale=alt.Scale(domain=['Positive', 'Neutral', 'Negative'], range=['blue', 'grey', 'red']))
     )
 
