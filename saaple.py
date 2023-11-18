905b1f43bb917d2c947de6147184332431352c5d
Sobang419 <109142073+Sobang419@users.noreply.github.com>
2023-11-18 18:15:09 +0900

    Update saaple.py

--- saaple.py
+++ saaple.py
@@ -14,8 +14,14 @@ st.title("뉴스 카테고리 및 감정 분포")
 
 # 사용자 입력 받기
 selected_date = st.selectbox("날짜 선택", df['datetime'].unique())
-unique_stock_codes = df['stock_code'].drop_duplicates().sort_values()
-selected_stock_code = st.selectbox("주식 코드 선택", unique_stock_codes)
+
+# 주식 코드 직접 입력 또는 선택
+input_stock_code = st.text_input("주식 코드 직접 입력")
+if input_stock_code:
+    selected_stock_code = input_stock_code
+else:
+    unique_stock_codes = df['stock_code'].drop_duplicates().sort_values()
+    selected_stock_code = st.selectbox("주식 코드 선택", unique_stock_codes)
 
 # 필터링된 데이터
 filtered_data = df[(df['datetime'] == selected_date) & (df['stock_code'] == selected_stock_code)]
